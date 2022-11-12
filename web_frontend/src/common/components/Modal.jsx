489de3ff61c58d9b755c1939904163554e95670a
Masi <51009658+MasiMasinga@users.noreply.github.com>
2022-11-12 15:40:57 +0200

    fix - backend init

--- web_frontend/src/common/components/Modal.jsx
+++ web_frontend/src/common/components/Modal.jsx
@@ -1,8 +1,64 @@
 import React from 'react'
 
-const Modal = () => {
+const Modal = ({
+  taskTitle,
+  taskDescription,
+  onCancel,
+  onhandleClose,
+  onSubmit,
+  buttonTitle,
+}) => {
   return (
-    <div>Modal</div>
+    <React.Fragment>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none" >
+        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+              <h3 className="text-3xl font-semibold text-black">
+                {taskTitle}
+              </h3>
+              <button
+                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                onClick={onCancel}
+              >
+                <span className="font-bold text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
+                  X
+                </span>
+              </button>
+            </div>
+            <div className="relative p-6 flex-auto">
+              <p className="my-4 text-black text-lg leading-relaxed">
+                {taskDescription}
+              </p>
+              <div className="mb-6">
+                <input
+                  type="text"
+                  id="large-input"
+                  className="block p-4 w-full text-black-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+              </div>
+            </div>
+            <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+              <button
+                className="text-blue-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={onhandleClose}
+              >
+                Close
+              </button>
+              <button
+                className="bg-blue-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={onSubmit}
+              >
+                {buttonTitle}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    </React.Fragment>
   )
 }
 
