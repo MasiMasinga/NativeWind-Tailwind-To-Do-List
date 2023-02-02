14383aae7ccda8faf2f86caa57a3ccc4be36c8fb
Masi <51009658+MasiMasinga@users.noreply.github.com>
2023-02-02 15:42:17 +0200

    fix - clean up

--- web_frontend/src/hooks/useRequestAuth.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const useRequestAuth = () => {
-  return (
-    <div>useRequestAuth</div>
-  )
-}
-
-export default useRequestAuth
--- web_frontend/src/hooks/useRequestResource.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const useRequestResource = () => {
-  return (
-    <div>useRequestResource</div>
-  )
-}
-
-export default useRequestResource
